b6203685a8adf1502b60cc1e1b780e686e028823
Arman KZ <armansauyenov@gmail.com>
2018-06-01 18:45:03 +0600

    Add project files.

---
+++ ClassStack/Staff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    public class Staff
+    {
+        Random rand = new Random();
+        public Staff(string _fullName, bool _gender, int _age, int _salarySize)
+        {
+            fullName = _fullName;
+            //fullName = "XJ"+rand.Next().ToString();
+            //pol = age < 63 ? pol = false : pol = true;
+            gender = _gender;
+            age = _age;
+            salarySize = _salarySize;
+        }
+        public string fullName { get; set; }
+        public bool gender { get; set; }
+        public int age { get; set; }
+        public int salarySize { get; set; }
+    }
+}
